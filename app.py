82739c291545234e834684a1ffee182208e671d2
Fyodor Sizov <f.sizov@yandex.ru>
2021-02-18 21:18:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def get_dataset_profile(dataset_name):
     valid_matches = len(ds_file["nk:datasetContent"]["items"])
     del ds_file
     return {
-        "preposition": prep,
+        "preposition": meta["prepositionVariants"],
         "property": criterion,
         "matches": {
             "all": this_dataset["allMatches"],
