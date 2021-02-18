5633b478561203524f98ec27b4afe7a357e260e8
Fyodor Sizov <f.sizov@yandex.ru>
2021-02-18 13:52:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/datasets")
+def show_datasets():
+    return send_from_directory("files/ui/datasets.html")
