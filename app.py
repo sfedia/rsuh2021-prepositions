b8eeb7975647690c4393adaaeff80f474c4f1016
Fyodor Sizov <f.sizov@yandex.ru>
2021-02-18 17:46:19 +0300

    +diachronic

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import jsonify
 from flask import send_from_directory
 from flask import render_template
+import json
 import os
 app = Flask(__name__, template_folder=os.path.abspath("files/ui/templates"))
 
@@ -23,3 +25,20 @@ def datasets_json_file(filename):
 @app.route("/dataset-profile/<dataset_name>")
 def show_dataset_profile(dataset_name):
     return render_template("dataset-profile.html")
+
+
+@app.route("/diachronic/<dataset_name>")
+def diachronic(dataset_name):
+    j = json.loads(open(os.path.join("files/datasets", f"{dataset_name}.json"), encoding="utf-8")).read())
+    years = {}
+    for item in j["nk:datasetContent"]["items"]:
+        extractedDate = re.search(r"(\d{4})\)?$", item["title"])
+        if extractedDate:
+            yearValue = int(extractedDate.group(1))
+            if yearValue not in years:
+                years[yearValue] = 1
+            else:
+                years[yearValue] += 1
+     return jsonify({
+         "stats": [{"x": year, "y": years[year] if year in years else 0} for year in range(1800, 2021 + 1)]
+     })
